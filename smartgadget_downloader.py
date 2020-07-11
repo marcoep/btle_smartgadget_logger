11d4e55259910997a724a53990d01d8ed1f49de3
Marco Eppenberger <mail@mebg.ch>
2020-07-11 17:07:18 +0200

    First try with logging downloader.

--- smartgadget_downloader.py
+++ smartgadget_downloader.py
@@ -3,8 +3,9 @@
 Marco Eppenberger, 2020
 """
 
-from struct import unpack
+from struct import unpack, pack
 from datetime import datetime
+from time import time, sleep
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.events import EVENT_JOB_ERROR
@@ -32,6 +33,10 @@ class SmartGadgetDownlaoder(object):
         # gatttool
         self.btadapter = pygatt.GATTToolBackend()
 
+        # data storage
+        self.last_temps = []
+        self.last_humids = []
+
     def start(self):
         self.scheduler.start()
         self.lgr.info("Scheduler started.")
@@ -47,22 +52,65 @@ class SmartGadgetDownlaoder(object):
         self.lgr.error(str(event.exception))
         self.lgr.error("Traceback: " + str(event.traceback))
 
+    def _ms_timestamp(self):
+        return int(round(time() * 1000))
+
+    def _unpack_SH3T_logger_data(self, binary_data):
+        seq_num_binary = binary_data[0:4]
+        seq_num = unpack('I', seq_num_binary)[0]
+        values_binary = binary_data[4:]
+        values = unpack('f', values_binary)
+        return seq_num, values
+
+    def _retrieve_temperature_log(self, handle, binary_data):
+        print("Rx of {:d} bytes.".format(len(binary_data)))
+        seq_num, values = self._unpack_SH3T_logger_data(binary_data=binary_data)
+        print("Received temp log #{:d}: {}".format(seq_num, values))
+        self.last_temps.extend(values)
+
+    def _retrieve_humidity_log(self, handle, binary_data):
+        print("Rx of {:d} bytes.".format(len(binary_data)))
+        seq_num, values = self._unpack_SH3T_logger_data(binary_data=binary_data)
+        print("Received humid log #{:d}: {}".format(seq_num, values))
+        self.last_humids.extend(values)
+
     def _event_tick(self):
 
         try:
             self.btadapter.start()
             device = self.btadapter.connect('DA:F0:63:93:BE:97', address_type=BLEAddressType.random)
-            temperature_binary = device.char_read(SHT3X_TEMPERATURE_UUID)
-            humidity_binary = device.char_read(SHT3X_HUMIDITY_UUID)
+
+            # temperature_binary = device.char_read(SHT3X_TEMPERATURE_UUID)
+            # humidity_binary = device.char_read(SHT3X_HUMIDITY_UUID)
+
+            # step 1: subscribe to value characteristics
+            device.subscribe(SHT3X_TEMPERATURE_UUID, callback=self._retrieve_temperature_log)
+            device.subscribe(SHT3X_HUMIDITY_UUID, callback=self._retrieve_humidity_log)
+
+            # step 2: write host ms timestamp
+            device.char_write(SYNC_TIME_MS_UUID, pack('I', self._ms_timestamp()))
+
+            # step 3: set oldest timestamp to retrieve
+            device.char_write(OLDEST_TIMESTAMP_MS_UUID, pack('I', self._ms_timestamp() - 60000))  # = 1 min in ms
+
+            # step 4: trigger download
+            device.char_write(START_LOGGER_DOWNLOAD_UUID, pack('c', 1))
+
+            # wait until download is over
+            sleep(10)
+
         finally:
             self.btadapter.stop()
 
-        temperature = unpack('f', temperature_binary)[0]
-        humidity = unpack('f', humidity_binary)[0]
+        # temperature = unpack('f', temperature_binary)[0]
+        # humidity = unpack('f', humidity_binary)[0]
 
         timestamp = str('{date:%Y-%m-%d_%H%M%S}'.format(date=datetime.now()))
 
-        log_line = "{0:s},{1:.2f},{2:.2f}\n".format(timestamp, temperature, humidity)
-
         with open("values_log.txt", 'a') as fp:
-            fp.write(log_line)
+            for temp, humid in zip(self.last_temps, self.last_humids):
+                log_line = "{0:s},{1:.2f},{2:.2f}\n".format(timestamp, temp, humid)
+                fp.write(log_line)
+
+        self.last_temps = []
+        self.last_humids = []
