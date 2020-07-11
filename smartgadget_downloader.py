030782718d537db06a67a38c20be8652d6b4fb99
Marco Eppenberger <mail@mebg.ch>
2020-07-11 21:42:44 +0200

    Creating series of timestamps according to loggin interval

--- smartgadget_downloader.py
+++ smartgadget_downloader.py
@@ -4,8 +4,9 @@ Marco Eppenberger, 2020
 """
 
 from struct import unpack, pack, iter_unpack
-from datetime import datetime
-from time import time, sleep
+from datetime import datetime, timedelta
+from time import time
+from threading import Event
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.events import EVENT_JOB_ERROR
@@ -31,10 +32,13 @@ class SmartGadgetDownloader(object):
         self.scheduler.add_listener(self._on_job_error, mask=EVENT_JOB_ERROR)
 
         # gatttool
-        gattoollog = open('./gatttoollog.txt', 'ab')
-        self.btadapter = pygatt.GATTToolBackend(gatttool_logfile=gattoollog)
+        self.btadapter = pygatt.GATTToolBackend()
 
-        # data storage
+        # threading: wait until all data is downloaded
+        self.download_done = Event()
+        self.download_done.clear()
+
+        # data storage, newest events are first!
         self.last_temps = []
         self.last_humids = []
 
@@ -64,15 +68,18 @@ class SmartGadgetDownloader(object):
         return seq_num, values
 
     def _retrieve_temperature_log(self, handle, binary_data):
-        print("Rx of {:d} bytes.".format(len(binary_data)))
+        # print("Rx of {:d} bytes.".format(len(binary_data)))
         seq_num, values = self._unpack_SH3T_logger_data(binary_data=binary_data)
-        print("Received temp log #{:d}: {}".format(seq_num, values))
+        # print("Received temp log #{:d}: {}".format(seq_num, values))
         self.last_temps.extend(values)
+        # signal download done when we don't receive any more values. temperature values get received after humidities
+        if len(values) == 0:
+            self.download_done.set()
 
     def _retrieve_humidity_log(self, handle, binary_data):
-        print("Rx of {:d} bytes.".format(len(binary_data)))
+        # print("Rx of {:d} bytes.".format(len(binary_data)))
         seq_num, values = self._unpack_SH3T_logger_data(binary_data=binary_data)
-        print("Received humid log #{:d}: {}".format(seq_num, values))
+        # print("Received humid log #{:d}: {}".format(seq_num, values))
         self.last_humids.extend(values)
 
     def _event_tick(self):
@@ -84,7 +91,19 @@ class SmartGadgetDownloader(object):
             # temperature_binary = device.char_read(SHT3X_TEMPERATURE_UUID)
             # humidity_binary = device.char_read(SHT3X_HUMIDITY_UUID)
 
+            # step 0: read currently set logging interval
+            logging_interval_ms_uInt32 = device.char_read(LOGGER_INTERVAL_MS_UUID)
+            logging_interval_ms = unpack('I', logging_interval_ms_uInt32)[0]
+
             # step 1: subscribe to value characteristics
+
+            # for this to work, the pygatt module must be hotfixed!! they calculate the handle number wrongly
+            # within the subscribe method!
+            # in the file pygatt/device.py
+            # change the line to:
+            #    characteristic_config_handle = value_handle + 2
+            #                                                    here was a +1, which is wrong for the smartgadget
+
             device.subscribe(SHT3X_TEMPERATURE_UUID,
                              callback=self._retrieve_temperature_log,
                              wait_for_response=False)
@@ -96,34 +115,44 @@ class SmartGadgetDownloader(object):
             device.char_write(SYNC_TIME_MS_UUID, pack('Q', self._ms_timestamp()))
 
             # step 3: set oldest timestamp to retrieve
-            device.char_write(OLDEST_TIMESTAMP_MS_UUID, pack('Q', self._ms_timestamp() - 65000))  # = 1 min in ms
+            device.char_write(OLDEST_TIMESTAMP_MS_UUID, pack('Q', self._ms_timestamp() - 600000))  # = 10 min in ms
             # device.char_write(OLDEST_TIMESTAMP_MS_UUID, pack('Q', 0))
 
             # step 4: trigger download
             device.char_write(START_LOGGER_DOWNLOAD_UUID, pack('B', 1))
 
             # wait until download is over
-            input("Press enter to continue...")
+            self.download_done.wait(55.0)  # we should be done within 55s
 
             # step 5: disable upload
             device.char_write(START_LOGGER_DOWNLOAD_UUID, pack('B', 0))
 
-            # step 6: unsub
+            # step 6: unsubscribe
+            # the same logic as described above with changing the pygatt module applies here
             device.unsubscribe(SHT3X_TEMPERATURE_UUID, wait_for_response=False)
             device.unsubscribe(SHT3X_HUMIDITY_UUID, wait_for_response=False)
 
         finally:
-            print("Finally clause. Stopping bt adaptor.")
+            # print("Finally clause. Stopping bt adaptor.")
             self.btadapter.stop()
 
         # temperature = unpack('f', temperature_binary)[0]
         # humidity = unpack('f', humidity_binary)[0]
 
-        timestamp = str('{date:%Y-%m-%d_%H%M%S}'.format(date=datetime.now()))
+        # create current timestamps, newest value is first!
+        timestamp_now = datetime.now()
+        timestamps = [timestamp_now - timedelta(milliseconds=x * logging_interval_ms) for x in
+                      range(len(self.last_temps))]
+        timestamp_strings = ['{date:%Y-%m-%d_%H%M%S}'.format(date=ts) for ts in timestamps]
+
+        if len(self.last_temps) != len(self.last_humids):
+            self.lgr.error("Temperatures and humidities lists are not of equal length! Only keeping available pairs!")
 
         with open("values_log.txt", 'a') as fp:
-            for temp, humid in zip(self.last_temps, self.last_humids):
-                log_line = "{0:s},{1:.2f},{2:.2f}\n".format(timestamp, temp, humid)
+            for tss, temp, humid in zip(timestamp_strings.reverse(),
+                                        self.last_temps.reverse(),
+                                        self.last_humids.reverse()):
+                log_line = "{0:s},{1:.2f},{2:.2f}\n".format(tss, temp, humid)
                 fp.write(log_line)
 
         self.last_temps = []
